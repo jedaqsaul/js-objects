6cc457ae3c1b78ba513586c8abceac29d8a014ca
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-03-11 10:12:29 +0300

    Started javascript objects

---
+++ main.js
@@ -0,0 +1,25 @@
+const user2 = {
+    gender:'Male', 
+    age='24',
+    firstName:'Aquila',
+  surname: "Wafula",
+  lastName: "Jedidiah",
+  occupation: "Software developer",
+  isHumorous:false,
+  netWorth:13,
+  status:'single',
+  height:'5 7',
+  Nationality:'Kenyan',
+  phoneNumber:7050503614,
+  hobbes:[
+    'watching movies', 'Reading books', 'Twitter fanatic', 'playing basketball'
+
+  ],
+  school: {
+    name: "Moringa School",
+    course: "software engineering",
+  },
+};
+
+console.log(user2.surname);
+console.log(user2.school.name);
